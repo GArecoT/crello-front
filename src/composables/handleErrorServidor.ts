e723c9d5061437aec245db0feaa12698397acfc7
Gabriel Areco <areco23@gmail.com>
2026-03-25 14:07:39 -0400

    FEAT: Função central handleError

--- src/composables/handleErrorServidor.ts
+++ src/composables/handleErrorServidor.ts
@@ -1,8 +1,19 @@
+import { Notify } from "quasar";
 import type { Response, RespostaServidor } from "./tipos";
 
-export default function (error: unknown): Response {
+
+export default function (error: unknown, notify = false): Response {
   if (typeof error === 'object' && error !== null && 'response' in error) {
     const erroServidor = error as RespostaServidor
+
+    if (notify) {
+      Notify.create({
+        message: erroServidor.response.data.info.msg,
+        position: 'top-right',
+        timeout: 3000,
+        type: 'negative',
+      })
+    }
 
     return erroServidor.response.data
   } else {
